0976cf644776377807cfc62bb88564b307ec2434
Devesh Sawant <devesh47cool@gmail.com>
2018-04-24 23:52:23 +0530

    added setup.py and init for builds

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='bukmarker',
+      version='0.1',
+      description='Command line integrated bookmark manager',
+      url='https://github.com/dsaw/bukmarker',
+      author='Devesh Sawant',
+      author_email='devesh47cool@gmail.com',
+      license='GPLv3',
+      install_requires=[
+        'beautifulsoup4']
+      zip_safe=False
+)
